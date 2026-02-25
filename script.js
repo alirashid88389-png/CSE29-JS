549c3ec8af0789690a463e33eabe4b37962ec892
alirashid88389-png <alirashid88389@gmail.com>
2026-02-25 14:39:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hello World")
+document.write("hello World")
+
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+
+num = false;
+console.log(num);
+console.log(type(num));
+
+var num ="Rashid";
+console.log(num);
+console.log(typeof(num));
+
+var num =undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1 =symbole(4);
+console.log(sym1);
+
+const sym2 =symbol(4);
+console.log(sym1);
+if(sym==sym2){
+    console.log("true");
+}
+
+else{
+    console.log("false") //because each symbol is unique
+}
